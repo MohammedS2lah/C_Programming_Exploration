c7a947f3d3f1dfa5a821ac32966b41411833fbaa
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 19:29:15 +0300

    Update Problem3.c

--- Problem_Set1/Problem3.c
+++ Problem_Set1/Problem3.c
@@ -1,1 +1,17 @@
+//C Program to print a float number entered by the user
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num;
+
+    printf("\nEnter the Float number!\n");
+
+    scanf("%f", &num);
+
+    printf("\nThe Float number you entered is: %f\n", num);
+
+    return 0;
+}
