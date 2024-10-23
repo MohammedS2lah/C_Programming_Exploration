62df4a99835656c65eea68673c154b00478e8c1c
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 20:21:41 +0300

    Update Problem2.c

--- Problem_Set1/Problem2.c
+++ Problem_Set1/Problem2.c
@@ -1,17 +1,25 @@
-//C Program to print ASCII number of a given char
+/*
+    Tutorial: How to Write a C Program to Print the ASCII Value of a Character
+    In this tutorial, we will learn how to write a C program that takes a character as input from the user and prints its corresponding ASCII value. Understanding how to work with ASCII values is fundamental in C programming, as every character in C is internally represented by a numeric ASCII code.
+    
+    Problem Statement:
+    Write a C program that prompts the user to input a character and then displays the ASCII value of that character.
+*/
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  
+#include <stdlib.h> 
 
 int main()
 {
-    char charac;
+    char charac;  // Declare a variable of type 'char' to store the character input by the user
 
-    printf("\nEnter the character to get the ASCII!\n");
+    printf("\nEnter the character to get the ASCII!\n");  // Prompt the user for input
 
-    scanf("%c", &charac);
+    scanf("%c", &charac);  // The 'scanf' function reads the input character and stores it in the variable 'charac'
 
-    printf("\nThe ASCII number of the character %c is: %d\n", charac , charac);
+    printf("\nThe ASCII number of the character %c is: %d\n", charac, charac);  
+    // Prints both the character and its ASCII value. The format specifier '%c' displays the character, and '%d' prints its ASCII code.
 
-    return 0;
+    return 0;  
 }
+
