f4e5fa892761dd10397e4d0d7468042881d2ac0a
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 19:30:41 +0300

    Update Problem5.c

--- Problem_Set1/Problem5.c
+++ Problem_Set1/Problem5.c
@@ -1,1 +1,18 @@
+//C Program to print Hexa of a number
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+
+    printf("\n\tEnter an integer to represent in Hexadecimal: \n");
+
+    scanf("%d", &num);
+
+    printf("\n\tHexadecimal representation of %d is: %X\n", num, num);
+
+    return 0;
+}
 
