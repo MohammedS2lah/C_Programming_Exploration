57d9ecc9d4f7a7bdbf4749f2c3525dda03635430
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 20:25:57 +0300

    Update Problem5.c

--- Problem_Set1/Problem5.c
+++ Problem_Set1/Problem5.c
@@ -1,18 +1,23 @@
-//C Program to print Hexa of a number
+/*
+    Tutorial: How to Write a C Program to Print the Hexadecimal Representation of an Integer
+    In this tutorial, we will learn how to write a C program that takes an integer as input from the user and prints its hexadecimal (base-16) representation. Understanding number systems, like hexadecimal, is important when dealing with low-level programming, memory addressing, and bitwise operations.
+    
+    Problem Statement:
+    Write a C program that prompts the user to enter an integer and then displays its hexadecimal representation.
+*/
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  
+#include <stdlib.h> 
 
 int main()
 {
-    int num;
+    int num;  
+    printf("\n\tEnter an integer to represent in Hexadecimal: \n");  
 
-    printf("\n\tEnter an integer to represent in Hexadecimal: \n");
+    scanf("%d", &num); 
 
-    scanf("%d", &num);
+    printf("\n\tHexadecimal representation of %d is: %X\n", num, num);  
+    // Prints the hexadecimal representation of the integer. '%X' is used to print in uppercase hexadecimal format.
 
-    printf("\n\tHexadecimal representation of %d is: %X\n", num, num);
-
-    return 0;
+    return 0; 
 }
-
