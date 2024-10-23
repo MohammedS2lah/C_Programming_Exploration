d5c94ccc01b5175ba2806e2f22d2c87c2a7021e0
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 20:23:04 +0300

    Update Problem3.c

--- Problem_Set1/Problem3.c
+++ Problem_Set1/Problem3.c
@@ -1,17 +1,25 @@
-//C Program to print a float number entered by the user
-
-#include <stdio.h>
+/*
+    Tutorial: How to Write a C Program to Print a Float Number Entered by the User
+    In this tutorial, we will write a simple C program that reads a floating-point number (a number with decimal points) from the user and prints it back to the console. Working with different data types, like floats, is essential when dealing with real-world numerical data in programming.
+    
+    Problem Statement:
+    Write a C program that prompts the user to enter a float (decimal) number and then displays the entered number.
+*/
+    
+#include <stdio.h>  
 #include <stdlib.h>
 
 int main()
 {
-    float num;
+    float num;  // Declare a variable of type 'float' to store the floating-point number
 
-    printf("\nEnter the Float number!\n");
+    printf("\nEnter the Float number!\n");  
 
-    scanf("%f", &num);
+    scanf("%f", &num);  
 
-    printf("\nThe Float number you entered is: %f\n", num);
+    printf("\nThe Float number you entered is: %f\n", num);  
+    // Prints the floating-point number using the '%f' format specifier
 
-    return 0;
+    return 0; 
 }
+
