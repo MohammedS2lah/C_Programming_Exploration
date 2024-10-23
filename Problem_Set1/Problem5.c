e823349160061c6f77bc0aeab811933db9b58e9a
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 20:34:39 +0300

    Update Problem5.c

--- Problem_Set1/Problem5.c
+++ Problem_Set1/Problem5.c
@@ -1,6 +1,8 @@
 /*
     Tutorial: How to Write a C Program to Print the Hexadecimal Representation of an Integer
-    In this tutorial, we will learn how to write a C program that takes an integer as input from the user and prints its hexadecimal (base-16) representation. Understanding number systems, like hexadecimal, is important when dealing with low-level programming, memory addressing, and bitwise operations.
+    In this tutorial, we will learn how to write a C program that takes an integer as input from the user and prints its 
+    hexadecimal (base-16) representation. Understanding number systems, like hexadecimal, is important when dealing with 
+    low-level programming, memory addressing, and bitwise operations.
     
     Problem Statement:
     Write a C program that prompts the user to enter an integer and then displays its hexadecimal representation.
