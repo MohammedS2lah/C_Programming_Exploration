c90ff11d6a2ed1b6505578e997117ad925e16f05
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 19:31:49 +0300

    Update Problem7.c

--- Problem_Set1/Problem7.c
+++ Problem_Set1/Problem7.c
@@ -1,1 +1,21 @@
+/// C Program to compute Quotient and Remainder
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int int1, int2, Quotient, Remainder;
+
+    printf("\nEnter the two integers to compute Quotient and Remainder!\n");
+
+    scanf("%d %d", &int1, &int2);
+
+    Quotient = int1/int2;
+
+    Remainder = int1%int2;
+
+    printf("\nThe Quotient is: %d \t and the Remainder is: %d\n", Quotient, Remainder);
+
+    return 0;
+}
