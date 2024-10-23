103bdeedaa3d52a624f686105887a838a489e7a5
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 19:28:32 +0300

    Update Problem2.c

--- Problem_Set1/Problem2.c
+++ Problem_Set1/Problem2.c
@@ -1,1 +1,17 @@
+//C Program to print ASCII number of a given char
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char charac;
+
+    printf("\nEnter the character to get the ASCII!\n");
+
+    scanf("%c", &charac);
+
+    printf("\nThe ASCII number of the character %c is: %d\n", charac , charac);
+
+    return 0;
+}
