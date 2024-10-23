9daebe6414ae3c5e4d48f9857651d4dfeb243ecb
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-23 19:29:58 +0300

    Update Problem4.c

--- Problem_Set1/Problem4.c
+++ Problem_Set1/Problem4.c
@@ -1,1 +1,20 @@
+//C Program to add two integers
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int int1, int2, result;
+
+    printf("\nEnter the two integers to add!\n");
+
+    scanf("%d %d", &int1, &int2);
+
+    result = int1 + int2;
+
+    printf("\nThe result of adding the two integers %d and %d is: %d\n", int1 , int2, result);
+
+    return 0;
+}
 
