47c828bd9598d46031cfedcd1b96d25e4a9267f0
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-10-26 09:36:26 +0300

    Update Problem2.c

--- Problem_Set2/Problem2.c
+++ Problem_Set2/Problem2.c
@@ -1,1 +1,52 @@
+/*
+  Tutorial: Create a Simple Menu in C with User Choices
+  This tutorial will walk through a C program that displays a simple menu with three options. The user can choose to display 
+  "Good Morning," "Good Evening," or "Exit." The program uses a custom library function clrscr() to clear the screen each time a new menu is shown.
+  
+  Problem Statement:
+  Write a C program that repeatedly displays a menu with three options:
+  
+  Print "Good Morning"
+  Print "Good Evening"
+  Exit the program
+  
+  The program should clear the screen before each new menu display, and only stop when the user selects the "Exit" option.
+*/
 
+  
+#include <stdio.h>
+#include <stdlib.h>
+#include <MyLibrary.h>
+
+void main()
+{
+    char charac;
+    do
+    {
+    printf("\nSimple Menu:\n");
+    printf("------------\n");
+    printf("A-Good Morning \nB-Good Evening\nC-Exit \n");
+    printf("\nEnter your choice:");
+
+    charac = getchar();
+
+    switch(charac)
+    {
+        case 'A':   clrscr();
+                    printf("A-Good Morning");
+                    break;
+
+        case 'B':   clrscr();
+                    printf("B-Good Evening");
+                    break;
+
+        case 'C':
+                    clrscr();
+                    printf("C-Exit");
+                    break;
+    }
+    getch();
+    clrscr();
+    }
+    while (charac != 'C');
+}
