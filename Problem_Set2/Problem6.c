fc85ecbf50dfe49756a0c80dc385843cccbd0bd9
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-11-06 23:28:02 +0200

    Update Problem6.c

--- Problem_Set2/Problem6.c
+++ Problem_Set2/Problem6.c
@@ -1,20 +1,32 @@
-//C Program to Check Whether a Character is an Alphabet or not
+/*
+Tutorial: C Program to Check Whether a Character is an Alphabet
+In this tutorial, we will create a C program that checks whether a given character is an alphabet letter (A-Z or a-z) or not.
 
-#include <stdio.h>
-#include <stdlib.h>
+Problem Statement:
+Write a C program that reads a single character from the user and determines if it is an alphabet letter. 
+If the character is within the range of uppercase or lowercase alphabet characters, the program should output that it is an alphabet. 
+Otherwise, it should indicate that the character is not an alphabet.
+*/
+
+#include <stdio.h>    
+#include <stdlib.h>   
 
 void main()
 {
-    char charac;
+    char charac;    // Variable to store the input character
 
-    printf("Enter the Character to check\n");
+    printf("Enter the Character to check:\n");
 
-    charac=getchar();
+    charac = getchar();   // Read a single character input from the user
 
-    if(((charac >= 'A')&&(charac <= 'Z')) || ((charac >= 'a')&&(charac <= 'z')))
+    // Check if the character falls within the ranges of uppercase (A-Z) or lowercase (a-z) letters
+    if(((charac >= 'A') && (charac <= 'Z')) || ((charac >= 'a') && (charac <= 'z')))
     {
-        printf("The Character is an Alphabet");
+        printf("The Character is an Alphabet\n");
     }
     else
-        printf("The Character is not an Alphabet");
+    {
+        printf("The Character is not an Alphabet\n");
+    }
 }
+
